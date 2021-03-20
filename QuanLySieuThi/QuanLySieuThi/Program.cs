5cfc68015c0b983be7565b878557aa2fd4dfd1fd
LêMinhThành <68367028+minhthanh120@users.noreply.github.com>
2021-03-20 16:26:52 +0700

    update

--- QuanLySieuThi/QuanLySieuThi/Program.cs
+++ QuanLySieuThi/QuanLySieuThi/Program.cs
@@ -16,7 +16,7 @@ namespace QuanLySieuThi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_Khachhang());
+            Application.Run(new FormMain());
         }
     }
 }
