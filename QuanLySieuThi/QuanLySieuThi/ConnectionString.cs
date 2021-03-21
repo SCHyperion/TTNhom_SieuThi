bf238be29ba3cb991b55290315a884ad8b4648dd
VuHuyQuang278 <77908719+VuHuyQuang278@users.noreply.github.com>
2021-03-21 11:58:23 +0700

    update frmHoaDon, Them frmThongKe

--- QuanLySieuThi/QuanLySieuThi/ConnectionString.cs
+++ QuanLySieuThi/QuanLySieuThi/ConnectionString.cs
@@ -9,6 +9,6 @@ namespace QuanLySieuThi
     class ConnectionString
     {
         public static string str = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=QLSieuThi;Integrated Security=True";
-        //public static string conStr = @"Data Source=localhost;Initial Catalog=QLSieuThi;Integrated Security=True";
+        public static string conStr = @"Data Source=localhost;Initial Catalog=QLSieuThi;Integrated Security=True";
     }
 }
---
+++ QuanLySieuThi/QuanLySieuThi/frmThongKe.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace QuanLySieuThi
+{
+    partial class frmThongKe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnThongKeNgay = new System.Windows.Forms.Button();
+            this.txtNgay = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnThongKeNgay_Ngay = new System.Windows.Forms.Button();
+            this.txtNgay_Ngay2 = new System.Windows.Forms.TextBox();
+            this.txtNgay_Ngay1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(700, 450);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Gainsboro;
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(692, 421);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Thống kê sản phẩm theo ngày";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(49, 151);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(575, 247);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách mặt hàng";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(569, 226);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Khaki;
+            this.panel1.Controls.Add(this.btnThongKeNgay);
+            this.panel1.Controls.Add(this.txtNgay);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(49, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(572, 83);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnThongKeNgay
+            // 
+            this.btnThongKeNgay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThongKeNgay.Location = new System.Drawing.Point(360, 24);
+            this.btnThongKeNgay.Name = "btnThongKeNgay";
+            this.btnThongKeNgay.Size = new System.Drawing.Size(108, 33);
+            this.btnThongKeNgay.TabIndex = 2;
+            this.btnThongKeNgay.Text = "Thống kê";
+            this.btnThongKeNgay.UseVisualStyleBackColor = true;
+            this.btnThongKeNgay.Click += new System.EventHandler(this.btnThongKeNgay_Click);
+            // 
+            // txtNgay
+            // 
+            this.txtNgay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNgay.Location = new System.Drawing.Point(164, 30);
+            this.txtNgay.Name = "txtNgay";
+            this.txtNgay.Size = new System.Drawing.Size(151, 27);
+            this.txtNgay.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập ngày";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.MistyRose;
+            this.tabPage2.Controls.Add(this.groupBox2);
+            this.tabPage2.Controls.Add(this.panel2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(692, 421);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Thống kê sản phẩm theo khoảng thời gian";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView2);
+            this.groupBox2.Location = new System.Drawing.Point(49, 175);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(588, 224);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Chi tiết mặt hàng";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(582, 203);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkKhaki;
+            this.panel2.Controls.Add(this.btnThongKeNgay_Ngay);
+            this.panel2.Controls.Add(this.txtNgay_Ngay2);
+            this.panel2.Controls.Add(this.txtNgay_Ngay1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(105, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(432, 112);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnThongKeNgay_Ngay
+            // 
+            this.btnThongKeNgay_Ngay.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThongKeNgay_Ngay.Location = new System.Drawing.Point(274, 32);
+            this.btnThongKeNgay_Ngay.Name = "btnThongKeNgay_Ngay";
+            this.btnThongKeNgay_Ngay.Size = new System.Drawing.Size(106, 36);
+            this.btnThongKeNgay_Ngay.TabIndex = 4;
+            this.btnThongKeNgay_Ngay.Text = "Thống kê";
+            this.btnThongKeNgay_Ngay.UseVisualStyleBackColor = true;
+            this.btnThongKeNgay_Ngay.Click += new System.EventHandler(this.btnThongKeNgay_Ngay_Click);
+            // 
+            // txtNgay_Ngay2
+            // 
+            this.txtNgay_Ngay2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNgay_Ngay2.Location = new System.Drawing.Point(109, 63);
+            this.txtNgay_Ngay2.Name = "txtNgay_Ngay2";
+            this.txtNgay_Ngay2.Size = new System.Drawing.Size(136, 24);
+            this.txtNgay_Ngay2.TabIndex = 3;
+            // 
+            // txtNgay_Ngay1
+            // 
+            this.txtNgay_Ngay1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNgay_Ngay1.Location = new System.Drawing.Point(109, 20);
+            this.txtNgay_Ngay1.Name = "txtNgay_Ngay1";
+            this.txtNgay_Ngay1.Size = new System.Drawing.Size(136, 24);
+            this.txtNgay_Ngay1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Đến ngày";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Từ ngày";
+            // 
+            // frmThongKe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "frmThongKe";
+            this.Text = "Thống kê sản phẩm";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnThongKeNgay;
+        private System.Windows.Forms.TextBox txtNgay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnThongKeNgay_Ngay;
+        private System.Windows.Forms.TextBox txtNgay_Ngay2;
+        private System.Windows.Forms.TextBox txtNgay_Ngay1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
