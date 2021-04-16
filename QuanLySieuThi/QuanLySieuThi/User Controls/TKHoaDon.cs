fb69c93fa8ba943498483318a10c8471bd5122ea
VuHuyQuang278 <77908719+VuHuyQuang278@users.noreply.github.com>
2021-04-16 10:56:25 +0700

    Update

--- QuanLySieuThi/QuanLySieuThi/User Controls/TKHoaDon.cs
+++ QuanLySieuThi/QuanLySieuThi/User Controls/TKHoaDon.cs
@@ -25,6 +25,24 @@ namespace QuanLySieuThi.User_Controls
             txtMaHDBan.Text = HDBan.Rows[i].Cells[0].Value.ToString();
             txtMaKH.Text = HDBan.Rows[i].Cells[1].Value.ToString();
             dateTimeNgayMua.Text = HDBan.Rows[i].Cells[2].Value.ToString();
+
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+                SqlCommand cmd2 = new SqlCommand("SELECT MaHD AS N'Mã hóa đơn', MaHang AS N'Mã hàng', SoLuong AS N'Số lương' FROM CTHDBan WHERE MaHD = @mahd", connection);
+                cmd2.Parameters.AddWithValue("@mahd", txtMaHDBan.Text);
+                SqlDataReader read2 = cmd2.ExecuteReader();
+                DataTable table2 = new DataTable();
+                table2.Load(read2);
+                CTHDBan.DataSource = table2;
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void CTHDBan_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -40,6 +58,24 @@ namespace QuanLySieuThi.User_Controls
             int i = HDNhap.CurrentRow.Index;
             txtMaHDNhap.Text = HDNhap.Rows[i].Cells[0].Value.ToString();
             dateTimeNgayNhap.Text = HDNhap.Rows[i].Cells[1].Value.ToString();
+
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                    connection.Open();
+                SqlCommand cmd4 = new SqlCommand("SELECT MaHD AS N'Mã hóa đơn', MaHang AS N'Mã hàng', GiaNhap AS N'Giá nhập', Soluong AS N'Số lượng' FROM CTHDNhap WHERE MaHD = @mahd", connection);
+                cmd4.Parameters.AddWithValue("@mahd", txtMaHDNhap.Text);
+                SqlDataReader read4 = cmd4.ExecuteReader();
+                DataTable table4 = new DataTable();
+                table4.Load(read4);
+                CTHDNhap.DataSource = table4;
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void CTHDNhap_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -57,29 +93,29 @@ namespace QuanLySieuThi.User_Controls
             {
                 if (connection.State == ConnectionState.Closed)
                     connection.Open();
-                SqlCommand cmd1 = new SqlCommand("SELECT * FROM HDBan", connection);
+                SqlCommand cmd1 = new SqlCommand("SELECT MaHD AS N'Mã hóa đơn', MaKH AS N'Mã khách hàng', NgayMua AS N'Ngày mua' FROM HDBan", connection);
                 SqlDataReader read1 = cmd1.ExecuteReader();
                 DataTable table1 = new DataTable();
                 table1.Load(read1);
                 HDBan.DataSource = table1;
-
-                SqlCommand cmd2 = new SqlCommand("SELECT * FROM CTHDBan", connection);
+/*
+                SqlCommand cmd2 = new SqlCommand("SELECT MaHD AS N'Mã hóa đơn', MaHang AS N'Mã hàng', SoLuong AS N'Số lương' FROM CTHDBan", connection);
                 SqlDataReader read2 = cmd2.ExecuteReader();
                 DataTable table2 = new DataTable();
                 table2.Load(read2);
-                CTHDBan.DataSource = table2;
+                CTHDBan.DataSource = table2;*/
 
-                SqlCommand cmd3 = new SqlCommand("SELECT * FROM HDNhap", connection);
+                SqlCommand cmd3 = new SqlCommand("SELECT MaHD AS N'Mã hóa đơn', NgayNhap AS N'Ngày nhập' FROM HDNhap", connection);
                 SqlDataReader read3 = cmd3.ExecuteReader();
                 DataTable table3 = new DataTable();
                 table3.Load(read3);
                 HDNhap.DataSource = table3;
 
-                SqlCommand cmd4 = new SqlCommand("SELECT * FROM CTHDNhap", connection);
+                /*SqlCommand cmd4 = new SqlCommand("SELECT MaHD AS N'Mã hóa đơn', MaHang AS N'Mã hàng', GiaNhap AS N'Giá nhập', Soluong AS N'Số lượng' FROM CTHDNhap", connection);
                 SqlDataReader read4 = cmd4.ExecuteReader();
                 DataTable table4 = new DataTable();
                 table4.Load(read4);
-                CTHDNhap.DataSource = table4;
+                CTHDNhap.DataSource = table4;*/
                 if (connection.State == ConnectionState.Open)
                     connection.Close();
             }
