e89cb7b7f5400716d2f7e29012283926b8e6876b
quanganh <quanganh24112000@gmail.com>
2021-04-23 21:40:08 +0700

    QA

---
+++ QuanLySieuThi/QuanLySieuThi/Forms/Form2.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLySieuThi
+{
+    public partial class frmHDBan1 : Form
+    {
+        int ViTri = -1;
+
+        public frmHDBan1()
+        {
+            InitializeComponent();
+        }
+        void NapMaKH()
+        {
+            
+
+            string connectionStr = @"Data Source=DESKTOP-53IQ0S1\SQLEXPRESS;Initial Catalog=QLSieuThi;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionStr);
+            string query = "select  MaKH from KhachHang ";
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            DataTable data = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(data);
+            connection.Close();
+            cboMaKH.DataSource = data;
+            cboMaKH.ValueMember = "MaKH";
+            cboMaKH.DisplayMember = "MaKH";
+
+
+
+        }
+        void NapTenHang()
+        {
+            string connectionStr = @"Data Source=DESKTOP-53IQ0S1\SQLEXPRESS;Initial Catalog=QLSieuThi;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionStr);
+            string query = "select TenHang from MatHang  ";
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            DataTable data = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(data);
+            connection.Close();
+            TenHang.DataSource = data;
+            TenHang.ValueMember = "TenHang";
+            TenHang.DisplayMember = "TenHang";
+
+
+        }
+        void NapChiTietHD(string mhd)
+        {
+            
+            NapTenHang();
+
+            string connectionStr = @"Data Source=DESKTOP-53IQ0S1\SQLEXPRESS;Initial Catalog=QLSieuThi;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionStr);
+            string query = @"select CTHDBan.MaHD, CTHDBan.MaHang, MatHang.TenHang, CTHDBan.SoLuong, 
+                            MatHang.GiaBan,CTHDBan.SoLuong * MatHang.GiaBan as ThanhTien
+                            from CTHDBan, MatHang
+                            where MatHang.MaHang = CTHDBan.MaHang and MaHD = @MHD";
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.Clear();
+            command.Parameters.AddWithValue("@MHD", mhd);
+            DataTable data = new DataTable();
+
+            data.Clear();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(data);
+            dataGridView1.DataSource = data;
+            connection.Close();
+
+            //tinh tong tien
+            double tongtien = 0;
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                double sl = Convert.ToDouble(data.Rows[i]["SoLuong"]);
+                double gb = Convert.ToDouble(data.Rows[i]["GiaBan"]);
+                tongtien += sl * gb;
+
+            }
+            txtTongTien.Text = tongtien.ToString();
+
+
+
+
+
+
+
+
+        }
+
+        private DataTable doDuLieuRaDataTabe()
+        {
+            string connectionStr = @"Data Source=DESKTOP-53IQ0S1\SQLEXPRESS;Initial Catalog=QLSieuThi;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionStr);
+            string query = @"select * from HDBan";
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+
+            DataTable data = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+
+            adapter.Fill(data);
+
+
+            connection.Close();
+            return data;
+        }
+
+        
+        void DiChuyen(int i)
+        {
+            DataTable data = new DataTable();
+            
+            data = doDuLieuRaDataTabe();
+            txtMaHD.Text = data.Rows[i]["MaHD"].ToString();
+            dateTimePicker1.Value = DateTime.Now;
+
+            try
+            {
+                dateTimePicker1.Value = (DateTime)data.Rows[i]["NgayMua"];
+            }
+            catch { dateTimePicker1.Value = DateTime.Now; }
+            
+
+            cboMaKH.SelectedValue = data.Rows[i]["MaKH"].ToString();
+            //Hiện ra dữ liệu trong hóa đơn Bán tương ứng với MÃ Hóa Đơn = i
+        }
+
+
+
+
+       
+
+        private void button2_Click(object sender, EventArgs e)//button cuoi cung
+        {
+            DataTable data = new DataTable();
+            data = doDuLieuRaDataTabe();//dứ liệu trong bảng HdBan
+            if (ViTri < data.Rows.Count - 1)
+            {
+                ViTri = data.Rows.Count - 1;
+                DiChuyen(ViTri);
+                NapChiTietHD(txtMaHD.Text);
+
+            }
+
+
+        }
+
+       
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            DataTable data = new DataTable();
+            data = doDuLieuRaDataTabe();
+
+
+
+            NapMaKH();
+            NapTenHang();
+
+            // NapDanhSachHD();
+            if (data.Rows.Count > 0)
+            {
+                ViTri = 1;
+                // lấy trong database dòng thứ 2 
+                DiChuyen(ViTri);
+                NapChiTietHD(txtMaHD.Text);
+
+
+            }
+        }
+        
+
+        private void btnDauTien_Click(object sender, EventArgs e)
+        {
+            DataTable data = new DataTable();
+            data = doDuLieuRaDataTabe();
+            if (data.Rows.Count > 0)
+            {
+                ViTri = 0;
+                // lấy trong database dòng thứ 2 
+                DiChuyen(ViTri);
+                NapChiTietHD(txtMaHD.Text);
+
+
+            }
+        }
+
+        private void btnLen_Click(object sender, EventArgs e)
+        {
+            DataTable data = new DataTable();
+            data = doDuLieuRaDataTabe();
+            if (ViTri > 0)
+            {
+                ViTri -= 1;
+                DiChuyen(ViTri);
+                NapChiTietHD(txtMaHD.Text);
+
+
+            }
+        }
+
+        private void btnXuong_Click(object sender, EventArgs e)
+        {
+            DataTable data = new DataTable();
+            data = doDuLieuRaDataTabe();
+            if (ViTri < data.Rows.Count - 1)
+            {
+                ViTri += 1;
+                DiChuyen(ViTri);
+                NapChiTietHD(txtMaHD.Text);
+
+
+            }
+        }
+    }
+}
--- QuanLySieuThi/QuanLySieuThi/Forms/FormMain.Designer.cs
+++ QuanLySieuThi/QuanLySieuThi/Forms/FormMain.Designer.cs
@@ -64,8 +64,8 @@
             this.MenuItem_Help});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(873, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1164, 30);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -77,53 +77,53 @@
             this.MenuItem_Logout,
             this.MenuItem_Exit});
             this.hệThốngToolStripMenuItem.Name = "hệThốngToolStripMenuItem";
-            this.hệThốngToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.hệThốngToolStripMenuItem.Size = new System.Drawing.Size(88, 26);
             this.hệThốngToolStripMenuItem.Text = "Hệ Thống";
             // 
             // MenuItem_AccountDetail
             // 
             this.MenuItem_AccountDetail.Name = "MenuItem_AccountDetail";
-            this.MenuItem_AccountDetail.Size = new System.Drawing.Size(178, 22);
+            this.MenuItem_AccountDetail.Size = new System.Drawing.Size(220, 26);
             this.MenuItem_AccountDetail.Text = "Thông tin tài khoản";
             this.MenuItem_AccountDetail.Click += new System.EventHandler(this.MenuItem_AccountDetail_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(175, 6);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(217, 6);
             // 
             // MenuItem_Logout
             // 
             this.MenuItem_Logout.Name = "MenuItem_Logout";
-            this.MenuItem_Logout.Size = new System.Drawing.Size(178, 22);
+            this.MenuItem_Logout.Size = new System.Drawing.Size(220, 26);
             this.MenuItem_Logout.Text = "Đăng xuất";
             this.MenuItem_Logout.Click += new System.EventHandler(this.MenuItem_Logout_Click);
             // 
             // MenuItem_Exit
             // 
             this.MenuItem_Exit.Name = "MenuItem_Exit";
-            this.MenuItem_Exit.Size = new System.Drawing.Size(178, 22);
+            this.MenuItem_Exit.Size = new System.Drawing.Size(220, 26);
             this.MenuItem_Exit.Text = "Thoát";
             this.MenuItem_Exit.Click += new System.EventHandler(this.MenuItem_Exit_Click);
             // 
             // MenuItem_SanPham
             // 
             this.MenuItem_SanPham.Name = "MenuItem_SanPham";
-            this.MenuItem_SanPham.Size = new System.Drawing.Size(72, 20);
+            this.MenuItem_SanPham.Size = new System.Drawing.Size(88, 26);
             this.MenuItem_SanPham.Text = "Sản Phẩm";
             this.MenuItem_SanPham.Click += new System.EventHandler(this.MenuItem_SanPham_Click);
             // 
             // MenuItem_Customer
             // 
             this.MenuItem_Customer.Name = "MenuItem_Customer";
-            this.MenuItem_Customer.Size = new System.Drawing.Size(84, 20);
+            this.MenuItem_Customer.Size = new System.Drawing.Size(103, 26);
             this.MenuItem_Customer.Text = "Khách Hàng";
             this.MenuItem_Customer.Click += new System.EventHandler(this.MenuItem_Customer_Click);
             // 
             // MenuItem_Employee
             // 
             this.MenuItem_Employee.Name = "MenuItem_Employee";
-            this.MenuItem_Employee.Size = new System.Drawing.Size(74, 20);
+            this.MenuItem_Employee.Size = new System.Drawing.Size(91, 26);
             this.MenuItem_Employee.Text = "Nhân Viên";
             this.MenuItem_Employee.Click += new System.EventHandler(this.MenuItem_Employee_Click);
             // 
@@ -134,51 +134,53 @@
             this.MenuItem_Stat_DT,
             this.MenuItem_AllHD});
             this.MenuItem_Stat.Name = "MenuItem_Stat";
-            this.MenuItem_Stat.Size = new System.Drawing.Size(70, 20);
+            this.MenuItem_Stat.Size = new System.Drawing.Size(86, 26);
             this.MenuItem_Stat.Text = "Thống Kê";
             // 
             // MenuItem_Stat_SoSPBan
             // 
             this.MenuItem_Stat_SoSPBan.Name = "MenuItem_Stat_SoSPBan";
-            this.MenuItem_Stat_SoSPBan.Size = new System.Drawing.Size(195, 22);
+            this.MenuItem_Stat_SoSPBan.Size = new System.Drawing.Size(244, 26);
             this.MenuItem_Stat_SoSPBan.Text = "Số sản phẩm bán được";
             this.MenuItem_Stat_SoSPBan.Click += new System.EventHandler(this.MenuItem_Stat_SoSPBan_Click);
             // 
             // MenuItem_Stat_DT
             // 
             this.MenuItem_Stat_DT.Name = "MenuItem_Stat_DT";
-            this.MenuItem_Stat_DT.Size = new System.Drawing.Size(195, 22);
+            this.MenuItem_Stat_DT.Size = new System.Drawing.Size(244, 26);
             this.MenuItem_Stat_DT.Text = "Doanh thu";
             this.MenuItem_Stat_DT.Click += new System.EventHandler(this.MenuItem_Stat_DT_Click);
             // 
             // MenuItem_AllHD
             // 
             this.MenuItem_AllHD.Name = "MenuItem_AllHD";
-            this.MenuItem_AllHD.Size = new System.Drawing.Size(195, 22);
+            this.MenuItem_AllHD.Size = new System.Drawing.Size(244, 26);
             this.MenuItem_AllHD.Text = "Danh sách hóa đơn";
             this.MenuItem_AllHD.Click += new System.EventHandler(this.MenuItem_AllHD_Click);
             // 
             // MenuItem_CreateBill
             // 
             this.MenuItem_CreateBill.Name = "MenuItem_CreateBill";
-            this.MenuItem_CreateBill.Size = new System.Drawing.Size(86, 20);
+            this.MenuItem_CreateBill.Size = new System.Drawing.Size(107, 26);
             this.MenuItem_CreateBill.Text = "Tạo hóa đơn";
             this.MenuItem_CreateBill.Click += new System.EventHandler(this.MenuItem_CreateBill_Click);
             // 
             // MenuItem_Help
             // 
             this.MenuItem_Help.Name = "MenuItem_Help";
-            this.MenuItem_Help.Size = new System.Drawing.Size(65, 20);
+            this.MenuItem_Help.Size = new System.Drawing.Size(79, 26);
             this.MenuItem_Help.Text = "Trợ Giúp";
             this.MenuItem_Help.Click += new System.EventHandler(this.MenuItem_Help_Click);
             // 
             // statusStrip1
             // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.CurrentUser});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 495);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 609);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(873, 25);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1164, 31);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -187,29 +189,31 @@
             this.CurrentUser.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CurrentUser.ForeColor = System.Drawing.Color.Red;
             this.CurrentUser.Name = "CurrentUser";
-            this.CurrentUser.Size = new System.Drawing.Size(73, 20);
+            this.CurrentUser.Size = new System.Drawing.Size(93, 25);
             this.CurrentUser.Text = "Xin chào ";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = global::QuanLySieuThi.Properties.Resources.sieuthi;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 30);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(873, 471);
+            this.pictureBox1.Size = new System.Drawing.Size(1164, 579);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(873, 520);
+            this.ClientSize = new System.Drawing.Size(1164, 640);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hệ Thống Quản Lý Siêu Thị";
